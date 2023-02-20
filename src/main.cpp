7f653db9d3b2f8e73a0fc216128e634b897a4941
ericssonl07 <ljh07ericsson@gmail.com>
2023-02-20 19:40:22 +0800

    Added multiline comment at the top listing Base function declarations for ease of coding

--- src/main.cpp
+++ src/main.cpp
@@ -14,12 +14,18 @@ brain Brain;
 task controller_control;
 Bot base;
 
-int main() {
-  // initialize controller control
-  // task controller_control = task(control);
-  // void Move(double x, double y, double angle, double lengthTolerance = 25, double angleTolerance = 1, 
-  //			     double tickLength = 20, distanceUnits lengthUnit = mm, rotationUnits angleUnit = deg) 
+int main() {  
+  /*
+   * void Move(double x, double y, double angle, double lengthTolerance = 25, double angleTolerance = 1, 
+               double tickLength = 20, distanceUnits lengthUnit = mm, rotationUnits angleUnit = deg);
+	 * void Turn(double angle);
+	 * void SetHeading(double angle);
+	 * void Shoot(int seconds);
+	 * void Roll();
+  */
   base.Move(1200, 1200, 45, 25, 1, 20, mm, deg);
   base.SetHeading(69);
   base.Move(-1200, -1200, 75, 25, 1, 20, mm, deg);
+
+  // task controller_control = task(control);
 }
