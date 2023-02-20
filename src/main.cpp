f0a48421f53a1a9dbae7fa340a0f53891c9b1bb4
ericssonl07 <ljh07ericsson@gmail.com>
2023-02-20 19:38:00 +0800

    Added multiline comment at top showing function declarations for ease of programming

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,9 @@ Bot base;
 int main() {
   // initialize controller control
   // task controller_control = task(control);
-  base.Move(1200, 1200, 45, 5);
+  // void Move(double x, double y, double angle, double lengthTolerance = 25, double angleTolerance = 1, 
+  //			     double tickLength = 20, distanceUnits lengthUnit = mm, rotationUnits angleUnit = deg) 
+  base.Move(1200, 1200, 45, 25, 1, 20, mm, deg);
   base.SetHeading(69);
-  base.Move(-1200, -1200, 75, 5);
+  base.Move(-1200, -1200, 75, 25, 1, 20, mm, deg);
 }
