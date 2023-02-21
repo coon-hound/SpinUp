851a1d33b2a62ebb1c00e487296d73ef023f8502
ericssonl07 <ljh07ericsson@gmail.com>
2023-02-21 08:08:51 +0800

    Removed temperature throttling

--- src/control.cpp
+++ src/control.cpp
@@ -8,11 +8,6 @@
 
 using namespace vex;
 
-double throttlefactor(double tmp, const double maximumtmp) 
-{ 
-  return sqrt(1 - (pow(tmp, 10) / pow(maximumtmp, 10)));
-}
-
 int control() 
 {
   //initialization
@@ -48,11 +43,6 @@ int control()
   double left_motor1_speed, left_motor2_speed;
   double right_motor1_speed, right_motor2_speed;
 
-  double tmp[20];
-  const double maxTmp = 100;
-  double maxMotorTmp, maxIntakeTmp, maxFlywheelTmp;
-  for (int i = 0; i < 20; ++i) tmp[i] = 0;
-
   //main drive loop
   while (true) 
   {
@@ -75,30 +65,12 @@ int control()
       precisionFactor = 1.0;
     }
 
-    //motor temperature sensing
-    tmp[LEFT_MOTOR1]  = LeftMotor1.temperature(celsius);
-    tmp[LEFT_MOTOR2]  = LeftMotor2.temperature(celsius);
-    tmp[RIGHT_MOTOR1] = RightMotor1.temperature(celsius);
-    tmp[RIGHT_MOTOR2] = RightMotor2.temperature(celsius);
-    tmp[INTAKE1]      = Intake1.temperature(celsius);
-    tmp[INTAKE2]      = Intake2.temperature(celsius);
-    tmp[FLYWHEEL1]    = Flywheel1.temperature(celsius);
-    tmp[FLYWHEEL2]    = Flywheel2.temperature(celsius);
-
     //configure motor speeds
     left_motor1_speed = (axis3_value + axis4_value + axis1_value) / precisionFactor;
     left_motor2_speed = (axis3_value - axis4_value + axis1_value) / precisionFactor;
     right_motor1_speed = (axis3_value - axis4_value - axis1_value) / precisionFactor;
     right_motor2_speed = (axis3_value + axis4_value - axis1_value) / precisionFactor;
 
-    //throttling based on temperature
-    maxMotorTmp = std::max(tmp[LEFT_MOTOR1], 
-                  std::max(tmp[LEFT_MOTOR2], 
-                  std::max(tmp[RIGHT_MOTOR1], tmp[RIGHT_MOTOR2])));
-    left_motor1_speed *= throttlefactor(maxMotorTmp, maxTmp);
-    left_motor2_speed *= throttlefactor(maxMotorTmp, maxTmp);
-    right_motor1_speed *= throttlefactor(maxMotorTmp, maxTmp);
-    right_motor2_speed *= throttlefactor(maxMotorTmp, maxTmp);
     // spin motor
     LeftMotor1.spin(fwd, left_motor1_speed, pct);
     LeftMotor2.spin(fwd, left_motor2_speed, pct);
@@ -118,13 +90,13 @@ int control()
       // Intake.spin(fwd, intakespeed, pct);
     } 
 
-    if(!Controller.ButtonA.pressing() and !Controller.ButtonX.pressing())
+    if((!Controller.ButtonA.pressing()) and (!Controller.ButtonX.pressing()))
     {
       intakespeed = 0;
     }
       
     //flywheel speed management
-    if(Controller.ButtonR1.pressing()) 
+    if(Controller.ButtonUp.pressing()) 
     {
       flywheelspeed = 100;
     } 
@@ -132,32 +104,14 @@ int control()
     {
       flywheelspeed = 0;
     }
-
-    //throttling based on temperature
-    maxIntakeTmp = std::max(tmp[INTAKE1], tmp[INTAKE2]);
-    maxFlywheelTmp = std::max(tmp[FLYWHEEL1], tmp[FLYWHEEL2]);
-    intakespeed *= throttlefactor(maxIntakeTmp, maxTmp);
-    flywheelspeed *= throttlefactor(maxFlywheelTmp, maxTmp);
     
     //spinning
-    if (std::abs(intakespeed) >= 5.0) 
-    {
-    	Intake1.spin(fwd, intakespeed, pct);
-    	Intake2.spin(fwd, intakespeed, pct);
-    }
-    if (std::abs(flywheelspeed) >= 5.0) 
-    {
-      Flywheel1.spin(fwd, flywheelspeed, pct);
-    	Flywheel2.spin(fwd, flywheelspeed, pct);
-    }
-    //controller, brain alerts and information output
-    if (maxIntakeTmp / maxTmp > 0.75 or maxIntakeTmp / maxTmp > 0.75 or maxFlywheelTmp / maxTmp > 0.75) 
-    {
-      Controller.Screen.print("One or more motors overheating");
-      Controller.Screen.newLine();
-    }
-    std::cerr << "One or more motors overheating\n";
-    Controller.Screen.clearScreen();
+    Intake1.spin(fwd, intakespeed, pct);
+    Intake2.spin(fwd, intakespeed, pct);
+    Flywheel1.spin(fwd, flywheelspeed, pct);
+    Flywheel2.spin(fwd, flywheelspeed, pct);
+
+    std::cout << intakespeed << " " << flywheelspeed << "\n";
 
   }
   return 0;
